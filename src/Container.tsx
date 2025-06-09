9a9d288fa1feb35452938873e0d52f9b2711cb37
saraelainee <sara.duarte@estudante.ifms.edu.br>
2025-06-09 12:28:04 -0300

    Prop e State

---
+++ src/Container.tsx
@@ -0,0 +1,41 @@
+//Props -> é utilizado para passar um valor diferente para cada componente em sua criação 
+
+//States ->  Estados, salvam estados de variáveis do react e quando esses estados mudam 
+// automaticamente o React entende que ele precisa atualizar a pagina para mostrar 
+// os novos valores do componente
+
+//Class
+
+import React from 'react'
+interface ContainerProps {
+  nome:string
+}
+interface ContainerInfoState {
+  nome:string  
+}
+
+
+//Container é filha de React.Component
+// e por isso pode utilizar os métodos de React.Component
+class Container extends React.Component<ContainerProps,ContainerInfoState> {
+  constructor(props: ContainerProps) {
+    super(props)
+    this.state = {
+      nome: "Nome antes de Mudar"
+    }
+  }
+  mudar = () => {
+    this.setState ({nome: "Nome depois de Mudar"}) }
+
+  render(): React.ReactNode {
+    return (
+    <>
+    <h1>{this.props.nome}</h1>
+    <p>{this.state.nome}</p>
+    <button onClick={this.mudar}>Mudar Estado</button>
+    </>
+    
+  )
+  }
+}
+export default Container;
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
-import App from './App.tsx'
-import Header from './header.tsx'
-import Footer from './footer.tsx'
-import Body from './body.tsx'
+import Container from './Container.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    <Header />
-    <Body />
-    <Footer />
+    <Container nome='Header' />
+    <Container nome='Body' />
   </StrictMode>
 )
