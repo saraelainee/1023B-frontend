07ecfc68c8c011cfa8e2c5c813d8946c38d0e283
saraelainee <sara.duarte@estudante.ifms.edu.br>
2025-06-09 12:38:16 -0300

    Antes da aula acabar - inicio state

--- src/Container.tsx
+++ src/Container.tsx
@@ -1,41 +1,59 @@
-//Props -> é utilizado para passar um valor diferente para cada componente em sua criação 
+// //Props -> é utilizado para passar um valor diferente para cada componente em sua criação 
 
-//States ->  Estados, salvam estados de variáveis do react e quando esses estados mudam 
-// automaticamente o React entende que ele precisa atualizar a pagina para mostrar 
-// os novos valores do componente
+// //States ->  Estados, salvam estados de variáveis do react e quando esses estados mudam 
+// // automaticamente o React entende que ele precisa atualizar a pagina para mostrar 
+// // os novos valores do componente
 
-//Class
+// //Class
 
-import React from 'react'
-interface ContainerProps {
-  nome:string
-}
-interface ContainerInfoState {
-  nome:string  
-}
+// import React from 'react'
+// interface ContainerProps {
+//   nome:string
+// }
+// interface ContainerInfoState {
+//   nome:string  
+// }
 
 
-//Container é filha de React.Component
-// e por isso pode utilizar os métodos de React.Component
-class Container extends React.Component<ContainerProps,ContainerInfoState> {
-  constructor(props: ContainerProps) {
-    super(props)
-    this.state = {
-      nome: "Nome antes de Mudar"
-    }
-  }
-  mudar = () => {
-    this.setState ({nome: "Nome depois de Mudar"}) }
+// //Container é filha de React.Component
+// // e por isso pode utilizar os métodos de React.Component
+// class Container extends React.Component<ContainerProps,ContainerInfoState> {
+//   constructor(props: ContainerProps) {
+//     super(props)
+//     this.state = {
+//       nome: "Nome antes de Mudar"
+//     }
+//   }
+//   mudar = () => {
+//     this.setState ({nome: "Nome depois de Mudar"}) }
 
-  render(): React.ReactNode {
-    return (
-    <>
-    <h1>{this.props.nome}</h1>
-    <p>{this.state.nome}</p>
-    <button onClick={this.mudar}>Mudar Estado</button>
-    </>
+//   render(): React.ReactNode {
+//     return (
+//     <>
+//     <h1>{this.props.nome}</h1>
+//     <p>{this.state.nome}</p>
+//     <button onClick={this.mudar}>Mudar Estado</button>
+//     </>
     
-  )
+//   )
+//   }
+// }
+// export default Container;
+
+interface ContainerProps {
+  nome: string;
+}
+function Container(props:ContainerProps){
+  let texto = "Texto antes de Mudar";
+  function mudar() {
+    texto = "Texto depois de Mudar";
   }
+  return(
+    <>
+      <h1>{props.nome}</h1>
+      <p>{texto}</p>
+      <button onClick={mudar}>Mudar Estado</button>
+    </>
+  )
 }
 export default Container;
